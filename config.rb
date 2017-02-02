83c23b67cd2415b82a03d9a429f609c2fce8e8dd
Chris Singhavong <csinghavong@gmail.com>
2017-02-01 20:59:39 -0800

    feat(scss): Add config.rb for scss compiling

---
+++ config.rb
@@ -0,0 +1,31 @@
+# Require any additional compass plugins here.
+
+require 'compass/import-once/activate'
+
+# Set this to the root of your project when deployed:
+
+http_path = "/"
+
+# static asset dirs
+
+static_root = "src/static"
+
+css_dir = static_root + "/css/"
+sass_dir = static_root + "/scss/"
+images_dir = static_root + "/img/"
+javascripts_dir = static_root + "/js/"
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
